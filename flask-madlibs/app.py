2120270d6046ba15c532f7eb9ee7a55f146f9726
stocktons <sarah@thanksforallthe.fish>
2021-07-07 20:33:41 -0400

    reviewed by Brit and Joel

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
-from stories import excited_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -11,14 +10,13 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def home():
+    # docstring here (write these first in the future)
     return render_template("questions.html", prompts=silly_story.prompts)
 
 @app.route('/results')
 def results():
-
+    #docstring here
     story = silly_story.generate(request.args)
-    
-    
     return render_template("story.html", story=story)
 
 
