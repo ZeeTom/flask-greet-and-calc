36db0b97df89354a425eaeef46a228f61afc8a30
ZeeTom <zthom13@hotmail.com>
2021-07-07 18:18:03 -0600

    updates

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
+from stories import excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -14,4 +15,13 @@ def home():
 
 @app.route('/results')
 def results():
-    return render_template("story.html")
+
+    story = silly_story.generate(request.args)
+    
+    
+    return render_template("story.html", story=story)
+
+
+@app.route('/<story_type>')
+def chosen_story(story_type):
+    return render_template('questions.html', prompts=story_type.prompts)
